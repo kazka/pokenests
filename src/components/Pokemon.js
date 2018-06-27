3377d614d9fa5c4829a871d6d15432ebe98bee6a
Kaisa Kauppila <kaisa.kauppila@kuubi.fi>
2018-06-27 14:10:39 +0300

    added styles and pokemon fields

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Pokemon = ({pokemon}) => (
-  <div>
-    <p className="pokemon-name" style={{color: pokemon.color.name}}>{pokemon.name}</p>
+  <div className="pokemon" style={{borderColor: pokemon.color.name}}>
+    <p className="pokemon-name">{pokemon.name}</p>
+    <p><strong>Generation:</strong> {pokemon.generation.name}</p>
+    <p><strong>Habitat:</strong> {pokemon.habitat.name}</p>
+    <p><strong>Shape:</strong> {pokemon.shape.name}</p>
   </div>
 )
 
